ce333549f0d81a590126d784e6da4762efaf4ee1
Kirill Simonov <xi@resolvent.net>
2019-04-20 22:39:34 -0500

    make entity attributes public

--- src/entities.jl
+++ src/entities.jl
@@ -7,7 +7,7 @@
 @rectypes begin
 
 mutable struct PGCatalog
-    scm_map::Dict{String,PGSchema}
+    schemas::Dict{String,PGSchema}
 
     PGCatalog() =
         new(Dict{String,PGSchema}())
@@ -16,13 +16,13 @@ end
 mutable struct PGSchema
     linked::Bool
 
-    cat::PGCatalog
+    catalog::PGCatalog
     name::String
 
-    typ_map::Dict{String,PGType}
-    proc_map::Dict{Tuple{String,Vector{PGType}},PGProcedure}
-    tbl_map::Dict{String,PGTable}
-    seq_map::Dict{String,PGSequence}
+    types::Dict{String,PGType}
+    procedures::Dict{Tuple{String,Vector{PGType}},PGProcedure}
+    tables::Dict{String,PGTable}
+    sequences::Dict{String,PGSequence}
 
     PGSchema(cat, name) =
         new(false, cat, name,
@@ -35,11 +35,11 @@ end
 mutable struct PGType
     linked::Bool
 
-    scm::PGSchema
+    schema::PGSchema
     name::String
-    lbls::Union{Vector{String},Nothing}
+    labels::Union{Vector{String},Nothing}
 
-    dep_set::Set{Union{PGColumn,PGProcedure}}
+    dependencies::Set{Union{PGColumn,PGProcedure}}
 
     PGType(scm, name, lbls=nothing) =
         new(false, scm, name, lbls, Set{Union{PGColumn,PGProcedure}}())
@@ -48,11 +48,11 @@ end
 mutable struct PGProcedure
     linked::Bool
 
-    scm::PGSchema
+    schema::PGSchema
     name::String
-    typs::Vector{PGType}
-    ret_typ::PGType
-    src::String
+    types::Vector{PGType}
+    return_type::PGType
+    source::String
 
     PGProcedure(scm, name, typs, ret_typ, src) =
         new(false, scm, name, typs, ret_typ, src)
@@ -61,22 +61,21 @@ end
 mutable struct PGTable
     linked::Bool
 
-    scm::PGSchema
+    schema::PGSchema
     name::String
 
-    col_map::Dict{String,PGColumn}
-    col_seq::Vector{PGColumn}
+    columns::Dict{String,PGColumn}
 
     PGTable(scm, name) =
-        new(false, scm, name, Dict{String,PGColumn}(), PGColumn[])
+        new(false, scm, name, Dict{String,PGColumn}())
 end
 
 mutable struct PGColumn
     linked::Bool
 
-    tbl::PGTable
+    table::PGTable
     name::String
-    typ::PGType
+    type_::PGType
     not_null::Bool
     default::Union{String,Nothing}
 
@@ -87,7 +86,7 @@ end
 mutable struct PGSequence
     linked::Bool
 
-    scm::PGSchema
+    schema::PGSchema
     name::String
 
     PGSequence(scm, name) =
@@ -102,25 +101,17 @@ Base.show(io::IO, cat::PGCatalog) =
     print(io, "<DATABASE>")
 
 Base.getindex(cat::PGCatalog, name::AbstractString) =
-    get_schema(cat, name)
+    cat.schemas[name]
+
+Base.get(cat::PGCatalog, name::AbstractString, default) =
+    get(cat.schemas, name, default)
 
 Base.iterate(cat::PGCatalog, state...) =
-    iterate(list_schemas(cat), state...)
-
-get_schema(cat::PGCatalog, name::AbstractString) =
-    cat.scm_map[name]
-
-get_schema(cat::PGCatalog, name::AbstractString, default) =
-    get(cat.scm_map, name, default)
-
-list_schemas(cat::PGCatalog) =
-    values(cat.scm_map)
+    iterate(values(cat.schemas), state...)
 
 function add_schema!(cat::PGCatalog, name::AbstractString)
-    @assert !(name in keys(cat.scm_map))
     scm = PGSchema(cat, name)
     link!(scm)
-    scm
 end
 
 # Schema operations.
@@ -129,116 +120,79 @@ Base.show(io::IO, scm::PGSchema) =
     print(io, "<$(!scm.linked ? "DROPPED " : "")SCHEMA $(sql_name(scm.name))>")
 
 Base.getindex(scm::PGSchema, name::AbstractString) =
-    get_table(scm, name)
+    scm.tables[name]
+
+Base.get(scm::PGSchema, name::AbstractString, default) =
+    get(scm.tables, name, default)
 
 Base.iterate(scm::PGSchema, state...) =
-    iterate(list_tables(scm), state...)
+    iterate(values(scm.tables), state...)
 
 function link!(scm::PGSchema)
     @assert !scm.linked
-    scm.cat.scm_map[scm.name] = scm
+    @assert !(scm.name in keys(scm.catalog.schemas))
+    scm.catalog.schemas[scm.name] = scm
     scm.linked = true
     scm
 end
 
 function unlink!(scm::PGSchema)
     @assert scm.linked
-    delete!(scm.cat.scm_map, scm.name)
+    delete!(scm.catalog.schemas, scm.name)
     scm.linked = false
     scm
 end
 
 function remove!(scm::PGSchema)
     @assert scm.linked
-    for tbl in list_tables(scm)
+    for tbl in values(scm.tables)
         remove!(tbl)
     end
-    for typ in list_types(scm)
+    for typ in values(scm.types)
         remove!(typ)
     end
     unlink!(scm)
-    scm.cat
-end
-
-get_catalog(scm::PGSchema) =
-    scm.cat
-
-get_name(scm::PGSchema) =
-    scm.name
-
-function set_name!(scm::PGSchema, name::AbstractString)
-    @assert scm.linked
-    name != scm.name || return scm
-    @assert !(name in keys(scm.cat.scm_map))
-    unlink!(scm)
-    scm.name = name
-    link!(scm)
-    scm
+    scm.catalog
 end
 
 get_fullname(scm::PGSchema) =
-    (get_name(scm),)
+    (scm.name,)
 
-get_type(scm::PGSchema, name::AbstractString) =
-    scm.typ_map[name]
-
-get_type(scm::PGSchema, name::AbstractString, default) =
-    get(scm.typ_map, name, default)
-
-list_types(scm::PGSchema) =
-    values(scm.typ_map)
+function set_name!(scm::PGSchema, name::AbstractString)
+    @assert scm.linked
+    unlink!(scm)
+    scm.name = name
+    link!(scm)
+end
 
 function add_type!(scm::PGSchema, name::AbstractString, lbls::Union{AbstractVector{<:AbstractString},Nothing}=nothing)
     @assert scm.linked
-    @assert !(name in keys(scm.typ_map))
     typ = PGType(scm, name, lbls)
     link!(typ)
-    typ
 end
-
-list_procedures(scm::PGSchema) =
-    values(scm.proc_map)
 
 function add_procedure!(scm::PGSchema, name::AbstractString, typs::Vector{PGType}, ret_typ::PGType, src::AbstractString)
     @assert scm.linked
-    @assert !((name, typs) in keys(scm.proc_map))
+    foreach(typs) do typ
+        @assert typ.linked
+        @assert typ.schema.catalog === scm.catalog
+    end
+    @assert ret_typ.linked
+    @assert ret_typ.schema.catalog === scm.catalog
     proc = PGProcedure(scm, name, typs, ret_typ, src)
     link!(proc)
-    proc
 end
-
-get_table(scm::PGSchema, name::AbstractString) =
-    scm.tbl_map[name]
-
-get_table(scm::PGSchema, name::AbstractString, default) =
-    get(scm.tbl_map, name, default)
-
-list_tables(scm::PGSchema) =
-    values(scm.tbl_map)
 
 function add_table!(scm::PGSchema, name::AbstractString)
     @assert scm.linked
-    @assert !(name in keys(scm.tbl_map))
     tbl = PGTable(scm, name)
     link!(tbl)
-    tbl
 end
-
-get_sequence(scm::PGSchema, name::AbstractString) =
-    scm.seq_map[name]
-
-get_sequence(scm::PGSchema, name::AbstractString, default) =
-    get(scm.seq_map, name, default)
-
-list_sequences(scm::PGSchema) =
-    values(scm.seq_map)
 
 function add_sequence!(scm::PGSchema, name::AbstractString)
     @assert scm.linked
-    @assert !(name in keys(scm.seq_map))
     seq = PGSequence(scm, name)
     link!(seq)
-    seq
 end
 
 # Type operations.
@@ -248,72 +202,63 @@ Base.show(io::IO, typ::PGType) =
 
 function link!(typ::PGType)
     @assert !typ.linked
-    typ.scm.typ_map[typ.name] = typ
+    @assert !(typ.name in keys(typ.schema.types))
+    typ.schema.types[typ.name] = typ
     typ.linked = true
     typ
 end
 
 function unlink!(typ::PGType)
     @assert typ.linked
-    delete!(typ.scm.typ_map, typ.name)
+    delete!(typ.schema.types, typ.name)
     typ.linked = false
     typ
 end
 
 function remove!(typ::PGType)
     @assert typ.linked
-    for col in list_columns(typ)
-        remove!(col)
+    for dep in typ.dependencies
+        remove!(dep)
     end
     unlink!(typ)
-    typ.scm
-end
-
-get_schema(typ::PGType) =
-    typ.scm
-
-get_name(typ::PGType) =
-    typ.name
-
-function set_name(typ::PGType, name::AbstractString)
-    @assert typ.linked
-    name != typ.name || return typ
-    @assert !(name in keys(typ.scm.typ_map))
-    unlink!(typ)
-    typ.name = name
-    link!(typ)
-    typ
+    typ.schema
 end
 
 get_fullname(typ::PGType) =
-    (get_fullname(get_schema(typ))..., get_name(typ))
+    (get_fullname(typ.schema)..., typ.name)
 
-get_labels(typ::PGType) =
-    typ.lbls
+function set_name(typ::PGType, name::AbstractString)
+    @assert typ.linked
+    unlink!(typ)
+    typ.name = name
+    link!(typ)
+end
 
 # Operations on stored procedures.
 
 Base.show(io::IO, proc::PGProcedure) =
-    print(io, "<$(!proc.linked ? "DROPPED " : "")PROCEDURE $(sql_name(get_fullname(proc)))($(sql_name(get_fullname.(proc.typs))))>")
+    print(io, "<$(!proc.linked ? "DROPPED " : "")PROCEDURE $(sql_name(get_fullname(proc)))($(sql_name(get_fullname.(proc.types))))>")
 
 function link!(proc::PGProcedure)
     @assert !proc.linked
-    proc.scm.proc_map[(proc.name, proc.typs)] = proc
-    for typ in proc.typs
-        push!(typ.dep_set, proc)
+    key = (proc.name, proc.types)
+    @assert !(key in keys(proc.schema.procedures))
+    proc.schema.procedures[key] = proc
+    for typ in proc.types
+        push!(typ.dependencies, proc)
     end
-    push!(proc.ret_typ.dep_set, proc)
+    push!(proc.return_type.dependencies, proc)
     proc.linked = true
     proc
 end
 
 function unlink!(proc::PGProcedure)
     @assert proc.linked
-    delete!(proc.ret_typ.dep_set, proc)
-    for typ in proc.typs
-        delete!(typ.dep_set, proc)
+    delete!(proc.return_type.dependencies, proc)
+    for typ in proc.types
+        delete!(typ.dependencies, proc)
     end
-    delete!(proc.scm.proc_map, proc.name)
+    delete!(proc.schema.procedures, (proc.name, proc.types))
     proc.linked = false
     proc
 end
@@ -321,36 +266,18 @@ end
 function remove!(proc::PGProcedure)
     @assert proc.linked
     unlink!(proc)
-    proc.scm
+    proc.schema
 end
-
-get_schema(proc::PGProcedure) =
-    proc.scm
-
-get_name(proc::PGProcedure) =
-    proc.name
 
 function set_name!(proc::PGProcedure, name::AbstractString)
     @assert proc.linked
-    name != proc.name || return tbl
-    @assert !(name in keys(tbl.scm.tbl_map))
-    unlink!(tbl)
-    tbl.name = name
-    link!(tbl)
-    tbl
+    unlink!(proc)
+    proc.name = name
+    link!(proc)
 end
 
 get_fullname(proc::PGProcedure) =
-    (get_fullname(get_schema(proc))..., get_name(proc))
-
-get_types(proc::PGProcedure) =
-    proc.typs
-
-get_return_type(proc::PGProcedure) =
-    proc.ret_typ
-
-get_source(proc::PGProcedure) =
-    proc.src
+    (get_fullname(proc.schema)..., proc.name)
 
 # Table operations.
 
@@ -358,91 +285,73 @@ Base.show(io::IO, tbl::PGTable) =
     print(io, "<$(!tbl.linked ? "DROPPED " : "")TABLE $(sql_name(get_fullname(tbl)))>")
 
 Base.getindex(tbl::PGTable, name::AbstractString) =
-    get_column(tbl, name)
+    tbl.columns[name]
+
+Base.get(tbl::PGTable, name::AbstractString, default) =
+    get(tbl.columns, name, default)
 
 Base.iterate(tbl::PGTable, state...) =
-    iterate(list_columns(tbl), state...)
+    iterate(values(tbl.columns), state...)
 
 function link!(tbl::PGTable)
     @assert !tbl.linked
-    tbl.scm.tbl_map[tbl.name] = tbl
+    @assert !(tbl.name in keys(tbl.schema.tables))
     tbl.linked = true
     tbl
 end
 
 function unlink!(tbl::PGTable)
     @assert tbl.linked
-    delete!(typ.scm.tbl_map, tbl.name)
+    delete!(tbl.schema.tables, tbl.name)
     tbl.linked = false
     tbl
 end
 
 function remove!(tbl::PGTable)
     @assert tbl.linked
-    for col in list_columns(tbl)
+    for col in values(tbl.columns)
         remove!(col)
     end
     unlink!(tbl)
-    tbl.scm
-end
-
-get_schema(tbl::PGTable) =
-    tbl.scm
-
-get_name(tbl::PGTable) =
-    tbl.name
-
-function set_name!(tbl::PGTable, name::AbstractString)
-    @assert tbl.linked
-    name != tbl.name || return tbl
-    @assert !(name in keys(tbl.scm.tbl_map))
-    unlink!(tbl)
-    tbl.name = name
-    link!(tbl)
-    tbl
+    tbl.schema
 end
 
 get_fullname(tbl::PGTable) =
-    (get_fullname(get_schema(tbl))..., get_name(tbl))
+    (get_fullname(tbl.schema)..., tbl.name)
 
-get_column(tbl::PGTable, name::AbstractString) =
-    tbl.col_map[name]
-
-get_column(tbl::PGTable, name::AbstractString, default) =
-    get(tbl.col_map, name, default)
-
-list_columns(tbl::PGTable) =
-    values(tbl.col_map)
+function set_name!(tbl::PGTable, name::AbstractString)
+    @assert tbl.linked
+    unlink!(tbl)
+    tbl.name = name
+    link!(tbl)
+end
 
 function add_column!(tbl::PGTable, name::AbstractString, typ::PGType, not_null::Bool)
     @assert tbl.linked
     @assert typ.linked
-    @assert tbl.scm.cat === typ.scm.cat
-    @assert !(name in keys(tbl.col_map))
+    @assert tbl.schema.catalog == typ.schema.catalog
     col = PGColumn(tbl, name, typ, not_null)
     link!(col)
-    col
 end
 
 # Column operations.
 
 Base.show(io::IO, col::PGColumn) =
-    print(io, "<$(!col.linked ? "DROPPED " : "")COLUMN $(sql_name(get_fullname(col))) $(sql_name(get_fullname(col.typ))) $(col.not_null ? "NOT " : "")NULL>")
+    print(io, "<$(!col.linked ? "DROPPED " : "")COLUMN $(sql_name(get_fullname(col))) $(sql_name(get_fullname(col.type_))) $(col.not_null ? "NOT " : "")NULL>")
 
 function link!(col::PGColumn)
     @assert !col.linked
-    col.tbl.col_map[col.name] = col
-    push!(col.tbl.col_seq, col)
-    push!(col.typ.dep_set, col)
+    @assert !(col.name in keys(col.table.columns))
+    col.table.columns[col.name] = col
+    push!(col.type_.dependencies, col)
     col.linked = true
     col
 end
 
 function unlink!(col::PGColumn)
     @assert col.linked
-    delete!(col.tbl.col_map, col.name)
-    filter!(!=(col), col.tbl.col_seq)
-    delete!(col.typ.dep_set, col)
+    delete!(col.type_.dependencies, col)
+    delete!(col.table.columns, col.name)
     col.linked = false
     col
 end
@@ -450,42 +359,33 @@ end
 function remove!(col::PGColumn)
     @assert col.linked
     unlink!(col)
-    col.tbl
-end
-
-get_table(col::PGColumn) =
-    col.tbl
-
-get_name(col::PGColumn) =
-    col.name
-
-function set_name!(col::PGColumn, name::AbstractString)
-    @assert col.linked
-    name != col.name || return col
-    @assert !(name in keys(col.tbl.col_map))
-    unlink!(col)
-    col.name = name
-    link!(col)
-    col
+    col.table
 end
 
 get_fullname(col::PGColumn) =
-    (get_fullname(get_table(col))..., get_name(col))
+    (get_fullname(col.table)..., col.name)
 
-get_type(col::PGColumn) =
-    col.typ
+function set_name!(col::PGColumn, name::AbstractString)
+    @assert col.linked
+    unlink!(col)
+    col.name = name
+    link!(col)
+end
 
-get_not_null(col::PGColumn) =
-    col.not_null
+function set_type!(col::PGColumn, typ::PGType)
+    @assert col.linked
+    @assert typ.linked
+    @assert col.schema.catalog === typ.schema.catalog
+    unlink!(col)
+    col.type_ = typ
+    link!(col)
+end
 
 function set_not_null!(col::PGColumn, not_null::Bool)
     @assert col.linked
     col.not_null = not_null
     col
 end
-
-get_default(col::PGColumn) =
-    col.default
 
 function set_default!(col::PGColumn, default::Union{AbstractString,Nothing})
     @assert col.linked
@@ -500,14 +400,15 @@ Base.show(io::IO, seq::PGSequence) =
 
 function link!(seq::PGSequence)
     @assert !seq.linked
-    seq.scm.seq_map[seq.name] = seq
+    @assert !(seq.name in keys(seq.schema.sequences))
+    seq.schema.sequences[seq.name] = seq
     seq.linked = true
     seq
 end
 
 function unlink!(seq::PGSequence)
     @assert seq.linked
-    delete!(seq.scm.seq_map, seq.name)
+    delete!(seq.schema.sequences, seq.name)
     seq.linked = false
     seq
 end
@@ -515,25 +416,17 @@ end
 function remove!(seq::PGSequence)
     @assert seq.linked
     unlink!(seq)
-    seq.scm
+    seq.schema
 end
 
-get_schema(seq::PGSequence) =
-    seq.scm
-
-get_name(seq::PGSequence) =
-    seq.name
+get_fullname(seq::PGSequence) =
+    (get_fullname(seq.schema)..., seq.name)
 
 function set_name!(seq::PGSequence, name::AbstractString)
     @assert seq.linked
-    name != seq.name || return tbl
-    @assert !(name in keys(seq.scm.seq_map))
     unlink!(seq)
     seq.name = name
     link!(seq)
     seq
 end
-
-get_fullname(seq::PGSequence) =
-    (get_fullname(get_schema(seq))..., get_name(seq))
 
--- src/virtual.jl
+++ src/virtual.jl
@@ -1,89 +1,47 @@
 #
-# Virtual properties.
+# Virtual properties and methods.
 #
 
 @inline Base.getproperty(cat::PGCatalog, prop::Symbol) =
-    if prop === :schema
-        (name, default...) -> get_schema(cat, name, default...)
-    elseif prop === :schemas
-        () -> list_schemas(cat)
-    else
-        getfield(cat, prop)
-    end
+    getfield(cat, prop)
 
 @inline Base.getproperty(scm::PGSchema, prop::Symbol) =
-    if prop === :catalog
-        get_catalog(scm)
-    elseif prop === :fullname
+    if prop === :fullname
         get_fullname(scm)
-    elseif prop === :type_
-        (name, default...) -> get_type(scm, name, default...)
-    elseif prop === :types
-        () -> list_types(scm)
-    elseif prop === :procedures
-        () -> list_procedures(scm)
-    elseif prop === :table
-        (name, default...) -> get_table(scm, name, default...)
-    elseif prop === :tables
-        () -> list_tables(scm)
-    elseif prop === :sequences
-        () -> list_sequences(scm)
     else
         getfield(scm, prop)
     end
 
 @inline Base.getproperty(typ::PGType, prop::Symbol) =
-    if prop === :schema
-        get_schema(typ)
-    elseif prop === :fullname
+    if prop === :fullname
         get_fullname(typ)
-    elseif prop === :labels
-        get_labels(typ)
-    elseif prop === :columns
-        () -> list_columns(typ)
     else
         getfield(typ, prop)
     end
 
 @inline Base.getproperty(proc::PGProcedure, prop::Symbol) =
-    if prop === :schema
-        get_schema(proc)
-    elseif prop === :fullname
+    if prop === :fullname
         get_fullname(proc)
-    elseif prop === :types
-        get_types(proc)
-    elseif prop == :return_type
-        get_return_type(proc)
     else
         getfield(proc, prop)
     end
 
 @inline Base.getproperty(tbl::PGTable, prop::Symbol) =
-    if prop === :schema
-        get_schema(tbl)
-    elseif prop === :fullname
+    if prop === :fullname
         get_fullname(tbl)
-    elseif prop === :column
-        (name, default...) -> get_column(tbl, name, default...)
-    elseif prop === :columns
-        () -> list_columns(tbl)
     else
         getfield(tbl, prop)
     end
 
 @inline Base.getproperty(col::PGColumn, prop::Symbol) =
-    if prop === :table
-        get_table(col)
-    elseif prop === :type_
-        get_type(col)
+    if prop === :fullname
+        get_fullname(col)
     else
         getfield(col, prop)
     end
 
 @inline Base.getproperty(seq::PGSequence, prop::Symbol) =
-    if prop === :schema
-        get_schema(seq)
-    elseif prop === :fullname
+    if prop === :fullname
         get_fullname(seq)
     else
         getfield(seq, prop)
